b71acfcd9bd8561457bcec84c8890c67edd0d448
dyeun96 <dyeun96@naver.com>
2014-09-25 13:40:26 +0900

    A

--- sketch.js
+++ sketch.js
@@ -1,8 +1,14 @@
 function setup()
 {
 	createCanvas(800, 600);
-	background(0);
+	
 	noFill();
-	strock(255, 0, 0);
-	ellipse(400, 300, 100, 100);
+	stroke(255, 0, 0);
+	
+}
+
+function draw()
+{
+	background(0);
+	ellipse(400, 300, mouseX, mouseY);
 }
